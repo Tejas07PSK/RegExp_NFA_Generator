333ee4554674719d31b92070c9832db19b675f1f
Palash Sarkar <palashsarkar0007@gmail.com>
2017-11-13 05:05:24 +0530

    Update Automata.java

--- src/assign/thomconc/pojo/Automata.java
+++ src/assign/thomconc/pojo/Automata.java
@@ -1,3 +1,19 @@
+/*  -> Simple Swing Automata Application.
+ *  -> Future Updates -- Android Port (Expected).
+ *  -> Project Type -- Self/Educational.
+ *  -> Institute -- University Institute Of Technology, Burdwan University.
+ *  -> Owner/Code file Designer :
+ *             @ Name - Palash Sarkar.
+ *             @ Department - Computer Science And Engineering.
+ *             @ Roll.Number - 2014_1038.
+ *             @ Email - palashsarkar0007@gmail.com.
+ *  -> Copyright Norms - Every piece of code given below 
+ *                       has been written by 'Palash Sarkar (Tj07)'©,
+ *                       and he holds the rights to the file. Not meant to be
+ *                       copied or tampered without prior permission
+ *                       from the author.             
+ */
+
 package assign.thomconc.pojo;
 
 import java.io.Serializable;
