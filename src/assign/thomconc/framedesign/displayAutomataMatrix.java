fb55d5671f3e75affbec43fbe043ad809f28ea26
Palash Sarkar <palashsarkar0007@gmail.com>
2017-12-15 13:32:03 +0530

    Update displayAutomataMatrix.java

--- src/assign/thomconc/framedesign/displayAutomataMatrix.java
+++ src/assign/thomconc/framedesign/displayAutomataMatrix.java
@@ -30,34 +30,35 @@ public class displayAutomataMatrix extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 
-	public displayAutomataMatrix(ArrayList <Automata> aut,String cre)
+	public displayAutomataMatrix ( ArrayList < Automata > aut, String cre )
 	{
-		super("NFA IN MATRIX FORM");
-		String heading[]={"Start State","End State","Edge Weight"};
-		String data[][];
-	    data = new String[aut.size()][3];
-	    int r=0;
-		for(Automata obj : aut)
+		
+		super( "NFA IN MATRIX FORM" );
+		String heading [] = { "Start State", "End State", "Edge Weight" };
+		String data [][];
+	        data = new String [ aut.size () ][ 3 ];
+	        int r = 0;
+		for ( Automata obj : aut )
 			{
-			     data[r][0]=obj.getStart();
-				 data[r][1]=obj.getEnd();
-				 data[r][2]=obj.getWeight();
-	             r++;
+			         data [ r ][ 0 ] = obj.getStart ();
+				 data [ r ][ 1 ] = obj.getEnd ();
+				 data [ r ][ 2 ] = obj.getWeight ();
+	                         r++;
 			}
 			
-	    Container con=getContentPane();
-	    con.setLayout(new BorderLayout());
+	        Container con = getContentPane ();
+	        con.setLayout ( new BorderLayout () );
 			
-	    JTable datatable=new JTable(data, heading);
-	    JScrollPane jsp=new JScrollPane(datatable);
+	        JTable datatable = new JTable ( data, heading );
+	        JScrollPane jsp = new JScrollPane ( datatable );
 			
-	    con.add(new JLabel("N.F.A. Matrix For R.E.- "+cre),BorderLayout.NORTH);
-		con.add(jsp,BorderLayout.CENTER);
+	        con.add ( new JLabel ( "N.F.A. Matrix For R.E.- " + cre ), BorderLayout.NORTH );
+		con.add ( jsp, BorderLayout.CENTER );
 			
-		setSize(850, 300);
+		setSize ( 850, 300 );
 			
-		setLocation(200, 200);
-		pack();
-		setVisible(true);
-    }
+		setLocation ( 200, 200 );
+		pack ();
+		setVisible ( true );
+        }
 }
