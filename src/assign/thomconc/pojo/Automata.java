d9b67b193a4976eccbbaebc1f43c56aae5d6dcfc
Palash Sarkar <palashsarkar0007@gmail.com>
2017-12-15 13:15:15 +0530

    Update Automata.java

--- src/assign/thomconc/pojo/Automata.java
+++ src/assign/thomconc/pojo/Automata.java
@@ -23,44 +23,44 @@ public class Automata implements Serializable
 	 private static final long serialVersionUID = 1L;
 	 
 	 private String start;
-     private String end;
-     private String weight;
+         private String end;
+         private String weight;
      
-     public Automata()
-     {
-    	 start="";
-    	 end="";
-    	 weight="";
-     }
+         public Automata ()
+         {
+    	     start = "";
+    	     end = "";
+    	     weight = "";
+         }
 
-	public String getStart() 
-	{
-		return (start);
-	}
+	 public String getStart () 
+	 {
+		return ( start );
+	 }
 
-	public void setStart(String start) 
-	{
+	 public void setStart ( String start ) 
+	 {
 		this.start = start;
-	}
+	 }
 
-	public String getEnd() 
-	{
-		return (end);
-	}
+	 public String getEnd () 
+	 {
+		return ( end );
+	 }
 
-	public void setEnd(String end) 
-	{
+	 public void setEnd ( String end ) 
+	 {
 		this.end = end;
-	}
+	 }
 
-	public String getWeight() 
-	{
-		return (weight);
-	}
+	 public String getWeight () 
+	 {
+		return ( weight );
+	 }
 
-	public void setWeight(String weight) 
-	{
+	 public void setWeight ( String weight ) 
+	 {
 		this.weight = weight;
-	}
+	 }
      
 }
